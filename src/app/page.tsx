fed1bc4d67423c31a052a8ebb4b5bbf9bd90772c
Thurnix <98490306+thurnix01@users.noreply.github.com>
2025-05-29 09:20:33 -0700

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -81,13 +81,13 @@ export default function Home() {
                     Victoria, British Columbia, Canada
                   </p>
                   <div className="flex gap-4 justify-center md:justify-start">
-                    <a href="mailto:contact@example.com" className="px-4 py-2 bg-[var(--primary)] hover:bg-[var(--primary-hover)] text-white rounded-md flex items-center gap-2 transition-all duration-300 ease-in-out hover:shadow-lg">
+                    <a href="mailto:absolondesigns@gmail.com" className="px-4 py-2 bg-[var(--primary)] hover:bg-[var(--primary-hover)] text-white rounded-md flex items-center gap-2 transition-all duration-300 ease-in-out hover:shadow-lg">
                       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="w-5 h-5">
                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
                       </svg>
                       Email Me
                     </a>
-                    <a href="https://linkedin.com" target="_blank" rel="noopener noreferrer" className="px-4 py-2 border border-[var(--card-border)] hover:bg-[var(--input-bg)] rounded-md flex items-center gap-2 hover:shadow-md">
+                    <a href="https://www.linkedin.com/in/terence-absolon/" target="_blank" rel="noopener noreferrer" className="px-4 py-2 border border-[var(--card-border)] hover:bg-[var(--input-bg)] rounded-md flex items-center gap-2 hover:shadow-md">
                       <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24" className="w-5 h-5">
                         <path d="M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667H9.351V9h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267 5.455v6.286zM5.337 7.433c-1.144 0-2.063-.926-2.063-2.065 0-1.138.92-2.063 2.063-2.063 1.14 0 2.064.925 2.064 2.063 0 1.139-.925 2.065-2.064 2.065zm1.782 13.019H3.555V9h3.564v11.452zM22.225 0H1.771C.792 0 0 .774 0 1.729v20.542C0 23.227.792 24 1.771 24h20.451C23.2 24 24 23.227 24 22.271V1.729C24 .774 23.2 0 22.222 0h.003z" />
                       </svg>
@@ -226,7 +226,7 @@ export default function Home() {
                         type="email" 
                         id="email" 
                         className="w-full px-4 py-2 border border-[var(--input-border)] rounded-md focus:outline-none focus:ring-2 focus:ring-[var(--primary)] bg-[var(--input-bg)] transition-all duration-300 ease-in-out" 
-                        placeholder="your.email@example.com"
+                        placeholder="absolondesigns@gmail.com"
                       />
                     </div>
                   </div>
