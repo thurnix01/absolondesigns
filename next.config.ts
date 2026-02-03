6ea16454461df60cd2b5710236f8ac61a5029685
Thurnix <98490306+thurnix01@users.noreply.github.com>
2026-02-02 21:18:57 -0800

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'export',
   // Update repository name to 'absolondesigns'
-  basePath: process.env.NODE_ENV === 'production' ? '/absolondesigns' : '',
+  basePath: process.env.NODE_ENV === 'production' ? '/' : '',
   images: {
     remotePatterns: [
       {
